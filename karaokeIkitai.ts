a5d787cbe8e6f18915e8dc8ea3ed78719bdcba7c
rettar <rettar.v@gmail.com>
2018-02-12 12:42:11 +0900

    first commit

---
+++ karaokeIkitai.ts
@@ -0,0 +1,37 @@
+import { OdnTweetData, OdnTweets } from "../../../odnTweets"
+import { OdnPlugins, OdnPluginResultData } from "../../../odnPlugins";
+import { Log } from "../../../odnUtils";
+
+export class KaraokeIkitai {
+  constructor(private tweetData: OdnTweetData, private fullName: string) {}
+
+  /**
+   * プラグインのメイン処理を実行
+   *
+   * @param {(isProcessed?: boolean) => void} finish
+   */
+  run(finish: (isProcessed?: boolean) => void) {
+    Log.d(this.tweetData.text);
+
+    const tweets = new OdnTweets(this.tweetData.accountData);
+    tweets.text = "ぼくもからおけいきたい";
+
+    // ツイートを投稿
+    tweets.postTweet((isSuccess) => {
+      Log.d(isSuccess ? "リプライに成功しました" : "リプライに失敗しました");
+      // 元ツイートをお気に入り登録
+      tweets.likeTweet();
+      finish();
+    });
+  }
+
+  /**
+   * プラグインを実行するかどうか判定
+   *
+   * @param {OdnTweetData} tweetData
+   * @returns {boolean}
+   */
+  static isValid(tweetData: OdnTweetData): boolean {
+    return tweetData.text.match(/^(からおけ|カラオケ|ひとから|ヒトカラ)(いきたい|行きたい)$/gi) ? true : false;
+  }
+}
